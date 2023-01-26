4fa6093512a3d3ee8ffeb560d7e453ce07edf68c
joynto1 <joynto92@gmail.com>
2023-01-26 20:42:59 +0600

    dynamic text

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+var typed = new Typed('.text-second', {
+    // Waits 1000ms after typing "First"
+    strings: [' Front End Developer',' Coder'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true,
+  });
+
 ////////////sidebar/////////////////
 function sidebar(){
     var navbar = document.getElementById("navbar");
-   setTimeout(function(){
     navbar.classList.toggle("show");
-   },
-  90
-   );
   };
   
+
