8644e6c9476ac9c6ec6dbba4fc377d2904566fb1
joynto1 <joynto92@gmail.com>
2023-03-20 21:34:13 +0600

    scroll revel effect

--- app.js
+++ app.js
@@ -12,5 +12,51 @@ function sidebar(){
     navbar.classList.toggle("show");
      
   };
+
+////////////////  Scroll Revel Effect //////////////////
+
+  ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:2500,
+    delay:300
+  }).reveal('#front_skills',{delay:500,origin:"bottom"});
+
+  ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:2500,
+    delay:300
+  }).reveal('#projects',{delay:2000,origin:"bottom"});
+
+  //////////////opore skills ///
+
+  ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:2500,
+    delay:300
+  }).reveal('.about_tittle',{delay:500,origin:"bottom"});
+  
+  ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:2500,
+    delay:300
+  }).reveal('.about_me',{delay:500,origin:"bottom"});
+  
+  ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:2500,
+    delay:300
+  }).reveal('form',{delay:500,origin:"right"});
+  
+  ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:2500,
+    delay:300
+  }).reveal('.profile',{delay:500,origin:"top"});
   
 
