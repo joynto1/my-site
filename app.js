0c3325853e49be3b2fb91c9430dddcfc99abe6f2
joynto1 <joynto92@gmail.com>
2023-03-21 21:17:56 +0600

    mm

--- app.js
+++ app.js
@@ -13,50 +13,47 @@ function sidebar(){
      
   };
 
-////////////////  Scroll Revel Effect //////////////////
+////////////
+// 
+////  Scroll Revel Effect //////////////////
 
-  ScrollReveal({
-    reset:true,
-    distance:"100px",
-    duration:2500,
-    delay:300
-  }).reveal('#front_skills',{delay:500,origin:"bottom"});
+// ScrollReveal({
+//   reset:true,
+//   distance:"100px",
+//   duration:2500,
+//   delay:300
+// }).reveal('#front_skills',{delay:500,origin:"bottom"});
 
-  ScrollReveal({
-    reset:true,
-    distance:"100px",
-    duration:2500,
-    delay:300
-  }).reveal('#projects',{delay:2000,origin:"bottom"});
+// ScrollReveal({
+//   reset:true,
+//   distance:"100px",
+//   duration:2500,
+//   delay:300
+// }).reveal('#projects',{delay:2000,origin:"bottom"});
 
-  //////////////opore skills ///
+// //////////////opore skills ///
 
-  ScrollReveal({
-    reset:true,
-    distance:"100px",
-    duration:2500,
-    delay:300
-  }).reveal('.about_tittle',{delay:500,origin:"bottom"});
-  
-  ScrollReveal({
-    reset:true,
-    distance:"100px",
-    duration:2500,
-    delay:300
-  }).reveal('.about_me',{delay:500,origin:"bottom"});
-  
-  ScrollReveal({
-    reset:true,
-    distance:"-100px",
-    duration:2500,
-    delay:300
-  }).reveal('form',{delay:500,origin:"left"});
-  
-  ScrollReveal({
-    reset:true,
-    distance:"100px",
-    duration:2500,
-    delay:300
-  }).reveal('.profile',{delay:500,origin:"top"});
-  
 
+// ScrollReveal({
+//   reset:true,
+//   distance:"100px",
+//   duration:2500,
+//   delay:300
+// }).reveal('.about_me',{delay:500,origin:"bottom"});
+
+// ScrollReveal({
+//   reset:true,
+//   distance:"-100px",
+//   duration:2500,
+//   delay:300
+// }).reveal('form',{delay:500,origin:"top"});
+
+// ScrollReveal({
+//   reset:true,
+//   distance:"100px",
+//   duration:2500,
+//   delay:300
+// }).reveal('.profile',{delay:500,origin:"top"});
+
+
+// // 
